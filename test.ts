778654ab6d0ec73d09458315608f535a91e0da12
Eugene Hauptmann <eugene@reactivelions.com>
2021-10-22 13:08:14 -0700

    released 0.0.13

--- test.ts
+++ test.ts
@@ -4,7 +4,7 @@ const DBL = `zen.spamhaus.org`
 const addresses = [`127.0.0.2`, `181.115.232.170`, `143.198.37.35`]
 
 addresses.map( (address:string) => {
-  dnsbl.lookup(address, DBL, {includeTxt:true})
+  dnsbl.lookupWithTxt(address, DBL)
   .then(res => {
     console.log(`response`, address, res)
   })
